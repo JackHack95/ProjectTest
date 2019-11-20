9b972aad88f582094c276bbefe57fe3091c3a2e4
Ikromjon Mamatov <i.mamatov95@gmail.com>
2019-11-20 09:59:09 +0500

    Initial Commit

---
+++ ProjectTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectTest
+//
+//  Created by Ikromjon Mamatov on 11/20/19.
+//  Copyright © 2019 Ikromjon Mamatov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
